98df881862882e73e2220c85f377678f133c35a1
victorsouzaleal <victorsouzaleal@gmail.com>
2024-04-12 13:42:04 -0300

    melhorias na exibição de erros e correção !simg

--- comandos/figurinhas.js
+++ comandos/figurinhas.js
@@ -1,12 +1,13 @@
 //REQUERINDO MÓDULOS
 const msgs_texto = require('../lib/msgs')
-const {erroComandoMsg, consoleErro, removerNegritoComando} = require("../lib/util")
-const sticker = require("../lib/sticker")
-const { Sticker, createSticker, StickerTypes } = require("wa-sticker-formatter")
+const {erroComandoMsg, consoleErro, removerNegritoComando, criarTexto} = require("../lib/util")
+const { Sticker, StickerTypes } = require("wa-sticker-formatter")
 const client = require("../lib-translate/baileys")
 const {MessageTypes}  = require("../lib-translate/msgtypes")
 const {downloadMediaMessage} = require('@whiskeysockets/baileys')
-const { duration } = require('moment-timezone')
+const fs = require('fs-extra')
+const path = require("path")
+const {stickerToPng} = require("../lib/conversao")
 
 
 module.exports = figurinhas = async(c,messageTranslated) => {
@@ -17,65 +18,85 @@ module.exports = figurinhas = async(c,messageTranslated) => {
         command = removerNegritoComando(command)
         const args =  commands.split(' ')
         switch(command){      
-            case '!s': 
-                if(isMedia || quotedMsg){
-                    var argSticker = args.length > 1 ? args[1].toLowerCase() : ""
-                    var stickerMetadata = {
-                        pack: `${process.env.NOME_AUTOR_FIGURINHAS.trim()} Stickers`, 
-                        author: process.env.NOME_AUTOR_FIGURINHAS.trim(),
-                        type: StickerTypes.CROPPED,
-                        quality: 100, 
-                        background: '#000000' 
-                    }
-
-                    if(argSticker == "1"){
-                        stickerMetadata.type = StickerTypes.CIRCLE
-                    } else if (argSticker == "2"){
-                        stickerMetadata.type = StickerTypes.FULL
-                    }
-
-                    var dadosMensagem = {
-                        tipo : (isMedia) ? type : quotedMsgObjInfo.type,
-                        mimetype : (isMedia)? mimetype : quotedMsgObjInfo.mimetype,
-                        message: (quotedMsg)? quotedMsgObj  : id,
-                        seconds: (quotedMsg)? quotedMsgObjInfo.seconds : seconds
-                    }
-
-                    if(dadosMensagem.tipo == MessageTypes.image){
-                        var bufferMessage = await downloadMediaMessage(dadosMensagem.message, "buffer")
-                        const stker = new Sticker(bufferMessage, stickerMetadata)
-                        await client.sendSticker(c,chatId, await stker.toMessage())
-                    } else if (dadosMensagem.tipo == MessageTypes.video){
-                        if(dadosMensagem.seconds < 11){
-                            stickerMetadata.quality = 5
+            case '!s':
+                try{
+                    if(isMedia || quotedMsg){
+                        var argSticker = args.length > 1 ? args[1].toLowerCase() : ""
+                        var stickerMetadata = {
+                            pack: `${process.env.NOME_AUTOR_FIGURINHAS.trim()} Stickers`, 
+                            author: process.env.NOME_AUTOR_FIGURINHAS.trim(),
+                            type: StickerTypes.CROPPED,
+                            quality: 100, 
+                            background: '#000000' 
+                        }
+    
+                        if(argSticker == "1"){
+                            stickerMetadata.type = StickerTypes.CIRCLE
+                        } else if (argSticker == "2"){
+                            stickerMetadata.type = StickerTypes.FULL
+                        }
+    
+                        var dadosMensagem = {
+                            tipo : (isMedia) ? type : quotedMsgObjInfo.type,
+                            mimetype : (isMedia)? mimetype : quotedMsgObjInfo.mimetype,
+                            message: (quotedMsg)? quotedMsgObj  : id,
+                            seconds: (quotedMsg)? quotedMsgObjInfo.seconds : seconds
+                        }
+    
+                        if(dadosMensagem.tipo == MessageTypes.image){
                             var bufferMessage = await downloadMediaMessage(dadosMensagem.message, "buffer")
                             const stker = new Sticker(bufferMessage, stickerMetadata)
                             await client.sendSticker(c,chatId, await stker.toMessage())
+                        } else if (dadosMensagem.tipo == MessageTypes.video){
+                            if(dadosMensagem.seconds < 11){
+                                stickerMetadata.quality = 5
+                                var bufferMessage = await downloadMediaMessage(dadosMensagem.message, "buffer")
+                                const stker = new Sticker(bufferMessage, stickerMetadata)
+                                await client.sendSticker(c,chatId, await stker.toMessage())
+                            } else {
+                                return client.reply(c,chatId, msgs_texto.figurinhas.sticker.video_invalido, id)
+                            }
                         } else {
-                            return client.reply(c,chatId, msgs_texto.figurinhas.sticker.video_invalido, id)
+                            return await client.reply(c, chatId, erroComandoMsg(command) , id)
                         }
                     } else {
                         return await client.reply(c, chatId, erroComandoMsg(command) , id)
                     }
-                } else {
-                    return await client.reply(c, chatId, erroComandoMsg(command) , id)
+                } catch(err){
+                    await client.reply(c, chatId, criarTexto(msgs_texto.geral.erro_comando_codigo, command), id)
+                    err.message = `${command} - ${err.message}`
+                    throw err
                 }
                 break
             
             case '!simg':
-                if(quotedMsg && quotedMsgObjInfo.type == MessageTypes.sticker){
-                    var quotedMessage = await client.getFakeQuotedMessage(messageId, sender, quotedMsgObj)
-                    var bufferMessage = await downloadMediaMessage(quotedMessage, "buffer")
-                    await c.sendMessage(chatId, {image: bufferMessage}).catch(()=>{
-                        client.reply(c, chatId, erroComandoMsg(command), id)
-                    })
-                } else {
-                    client.reply(c, chatId, erroComandoMsg(command), id)
+                try{
+                    if(quotedMsg && quotedMsgObjInfo.type == MessageTypes.sticker){
+                        var mensagemQuoted = quotedMsgObj, imagemSaida
+                        mensagemQuoted.message.stickerMessage.url = mensagemQuoted.message.stickerMessage.url == "https://web.whatsapp.net" ? `https://mmg.whatsapp.net${mensagemQuoted.message.stickerMessage.directPath}` : mensagemQuoted.message.stickerMessage.url
+                        var bufferMensagem = await downloadMediaMessage(mensagemQuoted, "buffer")
+                        var conversaoResultado = await stickerToPng(bufferMensagem)
+                        imagemSaida = conversaoResultado.saida
+                        if(!conversaoResultado.success){
+                            fs.unlinkSync(imagemSaida)
+                            return await client.reply(c, chatId, erroComandoMsg(command), id)
+                        } 
+                        await client.replyFile(c, MessageTypes.image, chatId, imagemSaida, '', id)
+                        fs.unlinkSync(imagemSaida)
+                    } else {
+                        await client.reply(c, chatId, erroComandoMsg(command), id)
+                    }
+                } catch(err){
+                    if(fs.existsSync(imagemSaida)) fs.unlinkSync(imagemSaida)
+                    await client.reply(c, chatId, criarTexto(msgs_texto.geral.erro_comando_codigo, command), id)
+                    err.message = `${command} - ${err.message}`
+                    throw err
                 }
+                
                 break      
         }
     } catch(err){
-        throw err
+        consoleErro(err, "FIGURINHAS")
     }
     
 
