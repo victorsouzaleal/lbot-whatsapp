61102f62c35db37f6261aa8b691580d03453e576
victorsouzaleal <victorsouzaleal@gmail.com>
2024-05-13 11:46:54 -0300

    corrigindo criacao do include.gypi

--- termux_reqs.sh
+++ termux_reqs.sh
@@ -2,8 +2,8 @@
 pkg update -y && pkg upgrade -y
 pkg install git -y && pkg install nodejs-lts -y && pkg install python -y && pkg install ffmpeg -y
 cd ~ && mkdir .gyp && cd .gyp
-echo '{
+echo "{
 	'variables': {
 		'android_ndk_path': ''
 	}
-}' >> ~/.gyp/include.gypi
+}" >> ~/.gyp/include.gypi
