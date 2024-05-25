3834480d58659dc2ab4bea91a496ce4666dd4c10
victorsouzaleal <victorsouzaleal@gmail.com>
2024-05-24 22:05:47 -0300

    verificar se o .gyp existe antes de tentar criar.

--- termux_reqs.sh
+++ termux_reqs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 pkg install nodejs-lts -y && pkg install python -y && pkg install ffmpeg -y && npm i -g yarn
-cd ~ && mkdir .gyp && cd .gyp
+cd ~  && [ ! -d ".gyp" ] && mkdir ~/.gyp
 echo "{
 	'variables': {
 		'android_ndk_path': ''
