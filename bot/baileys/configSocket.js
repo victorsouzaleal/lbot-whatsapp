8c6c65fe02857ad77ee11083f63815cea0df4da5
victorsouzaleal <victorsouzaleal@gmail.com>
2024-05-28 23:05:19 -0300

    Alteração na configuração do Socket Versão do WhatsApp Web foi atualizada para uma versão mais recente

--- bot/baileys/configSocket.js
+++ bot/baileys/configSocket.js
@@ -2,23 +2,22 @@ import pino from 'pino'
 import { MensagemControle } from '../controles/MensagemControle.js'
 import {isJidBroadcast, makeCacheableSignalKeyStore} from '@whiskeysockets/baileys'
 
-export default function configSocket (state, version, msgRetryCounterCache){
+export default function configSocket (state, cacheTentativasEnvio){
     return {
         printQRInTerminal: true,
         auth: {
             creds: state.creds,
             keys: makeCacheableSignalKeyStore(state.keys, pino({level : "silent"}))
         },
-        version,
-        msgRetryCounterCache,
-        keepAliveIntervalMs: 30000,
-        emitOwnEvents: true,
+        version : [2,3000,1013789583],
+        msgRetryCounterCache : cacheTentativasEnvio,
+        defaultQueryTimeoutMs: undefined,
         syncFullHistory: true,
         logger: pino({level : "silent"}),
         shouldIgnoreJid: jid => isJidBroadcast(jid) || jid?.endsWith('@newsletter'),
         getMessage: async (key) => {
             const mensagem = await new MensagemControle().recuperarMensagem(key.remoteJid, key.id)
-            return mensagem || undefined
+            return mensagem
         }
     }
 }
